6577f7d8c327de6b3364e0a2799872fa29adb897
paulo.santosDell <afonso.mea@gmail.com>
2022-06-06 22:50:05 -0300

    Adiciona FloatingButton, cria custom_app_bar.dart e adiciona na main.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/widgets/custom_app_bar.dart';
 
 class HomeScreen extends StatefulWidget {
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -9,8 +9,18 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final Size screenSize = MediaQuery.of(context).size;
 
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child: CustomAppBar(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[850],
+        child: const Icon(Icons.cast),
+        onPressed: () => print("Cast"),
+      ),
     );
   }
 }
