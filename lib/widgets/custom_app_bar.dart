9160c2f5aebf417be60b9511971440e784806e48
paulo.santosDell <afonso.mea@gmail.com>
2022-06-06 23:03:06 -0300

    Adiciona SafeArea no custom_app_bar.dart e logo Netflix

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/assets.dart';
 
 class CustomAppBar extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 24.0,
+      ),
+      color: Colors.orange,
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(Assets.netflixLogo0),
+          ],
+        ),
+      ),
+    );
   }
 }
