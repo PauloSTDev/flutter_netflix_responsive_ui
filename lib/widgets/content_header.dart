bcbc7ec2fd66fc00159bc76cf272ba3a7e8a7133
paulo.santosDell <afonso.mea@gmail.com>
2022-06-10 10:33:14 -0300

    Adiciona imagem de fundo, titulo e gradient

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/models/models.dart';
+import 'package:flutter_netflix_responsive_ui/widgets/vertical_icon_button.dart';
 
 class ContentHeader extends StatelessWidget {
   final Content featureContent;
@@ -11,6 +12,55 @@ class ContentHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          height: 500,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage(featureContent.imageUrl),
+            fit: BoxFit.cover,
+          )),
+        ),
+        Container(
+          height: 500,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black, Colors.transparent],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 110.0,
+          child: SizedBox(
+            width: 250.0,
+            child: Image.asset(featureContent.titleImageUrl),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 40.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              VerticalIconButton(
+                  icon: Icons.add,
+                  title: "List",
+                  onTap: () => print("list"),
+              ),
+              VerticalIconButton(
+                  icon: Icons.info_outline,
+                  title: "Info",
+                  onTap: () => print("Info"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
