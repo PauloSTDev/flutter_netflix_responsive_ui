f2ba127bccf708e58e5ae513fb81a363a326d0c6
paulo.santosDell <afonso.mea@gmail.com>
2022-07-06 15:19:20 -0300

    Cria nav_screen.dart

---
+++ lib/screens/nav_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NavScreen extends StatefulWidget {
+
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
